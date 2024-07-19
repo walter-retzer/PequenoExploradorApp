24705aaf85186d0def9749dcfcecc0920a18e7d5
walter <walter.d.retzer@gmail.com>
2024-07-19 12:14:25 -0300

    Implementação da SplashScreen com animação adicionada ao icone central

--- app/src/main/java/com/example/pequenoexploradorapp/MainActivity.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/MainActivity.kt
@@ -1,47 +1,46 @@
 package com.example.pequenoexploradorapp
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.example.pequenoexploradorapp.screen.SplashScreen
 import com.example.pequenoexploradorapp.ui.theme.PequenoExploradorAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val actionBar = actionBar
+        actionBar?.hide()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        window.statusBarColor= Color.BLACK
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
+
         enableEdgeToEdge()
         setContent {
             PequenoExploradorAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    SplashScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SplashScreenPreview() {
     PequenoExploradorAppTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
