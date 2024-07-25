4ec91e727d1abe519b586f85a3449a06c201dc1c
walter <walter.d.retzer@gmail.com>
2024-07-25 10:11:01 -0300

    Ajuste das imagens e layout das telas de WelcomeScreen

--- app/src/main/java/com/example/pequenoexploradorapp/screen/WelcomeScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/screen/WelcomeScreen.kt
@@ -9,11 +9,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -28,13 +25,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pequenoexploradorapp.R
 import com.example.pequenoexploradorapp.util.OnBoardingPage
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -52,7 +52,15 @@ fun WelcomeScreen() {
         pageCount = { 3 }
     )
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(id = R.drawable.simple_background),
+                contentScale = ContentScale.FillBounds
+            )
+
+    ) {
         HorizontalPager(
             modifier = Modifier.weight(2f),
             state = pagerState,
@@ -91,22 +99,25 @@ fun WelcomeScreen() {
 fun PagerScreen(onBoardingPage: OnBoardingPage) {
     Column(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxSize()
+            .padding(top = 80.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
         Image(
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .fillMaxHeight(0.7f),
             painter = painterResource(id = onBoardingPage.image),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(250.dp)
+                .clip(CircleShape),
             contentDescription = "Pager Image"
         )
         Text(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 20.dp),
             text = onBoardingPage.title,
-            fontSize = 14.sp,
+            fontSize = 22.sp,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
@@ -116,7 +127,7 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
                 .padding(horizontal = 40.dp)
                 .padding(top = 20.dp),
             text = onBoardingPage.description,
-            fontSize = 12.sp,
+            fontSize = 18.sp,
             fontWeight = FontWeight.Medium,
             textAlign = TextAlign.Center
         )
@@ -147,10 +158,9 @@ fun FinishButton(
                     contentColor = Color.White
                 )
             ) {
-                Text(text = "Finish")
+                Text(text = "Continuar")
             }
         }
-        Spacer(modifier.height(20.dp))
     }
 }
 
