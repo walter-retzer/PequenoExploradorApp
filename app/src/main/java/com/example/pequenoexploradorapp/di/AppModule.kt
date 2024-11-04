9b907b35fa472c9feb53bcb7135959d7717356eb
walter <walter.d.retzer@gmail.com>
2024-11-04 16:28:40 -0300

    Implementado koin

---
+++ app/src/main/java/com/example/pequenoexploradorapp/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.pequenoexploradorapp.di
+
+import com.example.pequenoexploradorapp.viewmodel.LoginUserViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
+
+val appModule = module {
+    viewModelOf(::LoginUserViewModel)
+}
+
