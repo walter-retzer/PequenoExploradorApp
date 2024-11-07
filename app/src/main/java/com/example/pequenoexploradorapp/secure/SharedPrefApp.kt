4ca4b42ef3749fa8bfb774c3e50b4a5365471cb3
walter <walter.d.retzer@gmail.com>
2024-11-07 11:20:19 -0300

    Configuração inicial do Shared Preferences

---
+++ app/src/main/java/com/example/pequenoexploradorapp/secure/SharedPrefApp.kt
@@ -0,0 +1,31 @@
+package com.example.pequenoexploradorapp.secure
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.pequenoexploradorapp.di.AppApplication
+
+class SharedPrefApp {
+    private val sharedPref: SharedPreferences = AppApplication
+        .appContext?.getSharedPreferences("explorador", Context.MODE_PRIVATE)
+        ?: throw IllegalArgumentException("Shared Preferences Error!")
+
+    fun saveBoolean(id: String, boolean: Boolean) {
+        sharedPref.edit()?.putBoolean(id, boolean)?.apply()
+    }
+
+    fun readBoolean(id: String): Boolean {
+        return sharedPref.getBoolean(id, false)
+    }
+
+    fun saveString(id: String, string: String) {
+        sharedPref.edit()?.putString(id, string)?.apply()
+    }
+
+    fun readString(id: String): String {
+        return sharedPref.getString(id, "") ?: ""
+    }
+
+    companion object {
+        val instance: SharedPrefApp by lazy { SharedPrefApp() }
+    }
+}
