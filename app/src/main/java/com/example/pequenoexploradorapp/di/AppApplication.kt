f67b65514228da4b29a646271a8801f14957f3d2
walter <walter.d.retzer@gmail.com>
2024-11-07 11:20:45 -0300

    Configuração do context para o Shared Preferences

--- app/src/main/java/com/example/pequenoexploradorapp/di/AppApplication.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/AppApplication.kt
@@ -1,6 +1,7 @@
 package com.example.pequenoexploradorapp.di
 
 import android.app.Application
+import android.content.Context
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -9,6 +10,8 @@ class AppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        appContext = applicationContext
+
         startKoin {
             androidLogger()
             androidContext(this@AppApplication)
@@ -16,6 +19,10 @@ class AppApplication : Application() {
                 appModule
             )
         }
+    }
+
+    companion object{
+        var appContext: Context? = null
     }
 
 }
