4deda034dd8550b98ce1df9d09e30e07ea25a66e
walter <walter.d.retzer@gmail.com>
2024-12-03 11:27:22 -0300

    Adicionado NetworkResult

---
+++ app/src/main/java/com/example/pequenoexploradorapp/network/ResultNetwork.kt
@@ -0,0 +1,13 @@
+package com.example.pequenoexploradorapp.network
+
+
+sealed class ResultNetwork<out T> {
+    data class Success<out T>(val data: T, val statusCode: Int) : ResultNetwork<T>()
+    data class Failure(val exception: Throwable, val statusCode: Int?) : ResultNetwork<Nothing>()
+
+    companion object {
+        fun <T> success(data: T, statusCode: Int): ResultNetwork<T> = Success(data, statusCode)
+        fun failure(exception: Throwable, statusCode: Int?): ResultNetwork<Nothing> =
+            Failure(exception, statusCode)
+    }
+}
