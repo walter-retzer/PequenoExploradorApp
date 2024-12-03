123d6e39314bd2847fb6b33a543c7c8bef091dec
walter <walter.d.retzer@gmail.com>
2024-12-03 10:47:35 -0300

    NetworkModule implementado utilizando Ktor

---
+++ app/src/main/java/com/example/pequenoexploradorapp/di/NetworkModule.kt
@@ -0,0 +1,70 @@
+package com.example.pequenoexploradorapp.di
+
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.accept
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+private const val NETWORK_TIME_OUT = 6_000L
+
+val provideHttpClientModule = module {
+    single {
+        HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                        prettyPrint = true
+                        isLenient = true
+                    },
+                    contentType = ContentType.Any
+                )
+            }
+
+            install(HttpTimeout) {
+                connectTimeoutMillis = NETWORK_TIME_OUT
+                requestTimeoutMillis = NETWORK_TIME_OUT
+                socketTimeoutMillis = NETWORK_TIME_OUT
+            }
+
+            install(Logging) {
+                logger = object : io.ktor.client.plugins.logging.Logger {
+                    override fun log(message: String) {
+                        Log.v("Logger Ktor => ", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+
+            install(ResponseObserver) {
+                onResponse { response ->
+                    Log.d("HTTP status:", "${response.status.value}")
+                }
+            }
+
+            install(DefaultRequest) {
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
+            }
+
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+                accept(ContentType.Application.Json)
+            }
+        }
+    }
+}
