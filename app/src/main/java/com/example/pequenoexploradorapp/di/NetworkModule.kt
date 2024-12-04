2a0f60596cf355406f309af13cf542df0912e1e2
walter <walter.d.retzer@gmail.com>
2024-12-04 12:08:17 -0300

    Ajustado networkModule

--- app/src/main/java/com/example/pequenoexploradorapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/NetworkModule.kt
@@ -1,19 +1,13 @@
 package com.example.pequenoexploradorapp.di
 
-import com.example.pequenoexploradorapp.network.ApiService
 import com.example.pequenoexploradorapp.network.httpClientAndroid
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
 val networkModule = module {
-    single { provideApiService(get()) }
     single { provideHttpClient() }
 }
 
 fun provideHttpClient(): HttpClient {
     return httpClientAndroid
-}
-
-fun provideApiService(httpClient: HttpClient): ApiService {
-    return ApiService(httpClient)
 }
