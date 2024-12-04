e323d69a64a5e0e4b097c2e45dc23bf657254dcd
walter <walter.d.retzer@gmail.com>
2024-12-04 10:36:54 -0300

    Configurado httpClient com o ktor

--- app/src/main/java/com/example/pequenoexploradorapp/network/ApiService.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/network/ApiService.kt
@@ -1,4 +1,4 @@
-package com.example.pequenoexploradorapp.api
+package com.example.pequenoexploradorapp.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
--- app/src/main/java/com/example/pequenoexploradorapp/network/HttpRoutes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pequenoexploradorapp.network
-
-object HttpRoutes {
-    const val BASE_URL_IMAGES = "https://images-api.nasa.gov/"
-    const val BASE_URL_ROVERS = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
-}
