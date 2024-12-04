d9dea6a28b1b20cec2f8fc8b5a914cd1c89d60fb
walter <walter.d.retzer@gmail.com>
2024-12-04 18:31:58 -0300

    Ajustado viewModelModules para o AndroidConnectivityObserver

--- app/src/main/java/com/example/pequenoexploradorapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/ViewModelModule.kt
@@ -1,10 +1,18 @@
 package com.example.pequenoexploradorapp.di
 
+import android.content.Context
+import com.example.pequenoexploradorapp.domain.connectivity.AndroidConnectivityObserver
 import com.example.pequenoexploradorapp.presentation.viewmodel.LoginUserViewModel
 import com.example.pequenoexploradorapp.presentation.viewmodel.SignInViewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val viewModelModules = module {
-    factory <LoginUserViewModel> { LoginUserViewModel() }
+    single { provideConnectivityStatus(androidApplication())}
+    factory <LoginUserViewModel> { LoginUserViewModel(provideConnectivityStatus(androidApplication())) }
     factory <SignInViewModel> {  SignInViewModel(get()) }
+}
+
+fun provideConnectivityStatus(client: Context): AndroidConnectivityObserver {
+    return AndroidConnectivityObserver(client)
 }
