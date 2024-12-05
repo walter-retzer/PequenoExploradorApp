2728d0947258f7da29effd0dca8789a9579a49d3
walter <walter.d.retzer@gmail.com>
2024-12-05 17:56:43 -0300

    Ajustado Rotas de navegação

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/navigation/Routes.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/navigation/Routes.kt
@@ -3,13 +3,27 @@ package com.example.pequenoexploradorapp.presentation.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-object SplashScreenRoute
+sealed class Route(
+    val route: String
+) {
+    @Serializable
+    object SplashScreenRoute : Route(route = "SplashScreen")
 
-@Serializable
-object WelcomeScreenRoute
+    @Serializable
+    object WelcomeScreenRoute : Route(route = "WelcomeScreen")
 
-@Serializable
-object LoginScreenRoute
+    @Serializable
+    object HomeScreenRoute : Route(route = "HomeScreen")
 
-@Serializable
-object SignInScreenRoute
+    @Serializable
+    object LoginScreenRoute : Route(route = "LoginScreen")
+
+    @Serializable
+    object SignInScreenRoute : Route(route = "SignInScreen")
+
+    @Serializable
+    object LoginGraphNav : Route(route = "LoginNavigation")
+
+    @Serializable
+    object HomeGraphNav : Route(route = "HomeNavigation")
+}
