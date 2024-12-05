09764cc9d61a2f54085a9492495d634ad64ce244
walter <walter.d.retzer@gmail.com>
2024-12-05 11:11:35 -0300

    Incluído KoinContext ao NavHostMain

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/navigation/NavHostMain.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/navigation/NavHostMain.kt
@@ -23,6 +23,7 @@ import com.example.pequenoexploradorapp.presentation.screen.WelcomeScreen
 import com.example.pequenoexploradorapp.domain.util.GoogleAuthUiClient
 import com.example.pequenoexploradorapp.presentation.viewmodel.LoginUserViewModel
 import kotlinx.coroutines.launch
+import org.koin.compose.KoinContext
 import org.koin.compose.koinInject
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -32,99 +33,101 @@ fun NavHostMain(
     googleAuthUiClient: GoogleAuthUiClient,
     context: Context
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = SplashScreenRoute,
-        enterTransition = NavAnimations.slideLeftEnterAnimation,
-        exitTransition = NavAnimations.slideLeftExitAnimation,
-        popEnterTransition = NavAnimations.popEnterRightAnimation,
-        popExitTransition = NavAnimations.popExitRightAnimation
-    ) {
-        composable<SplashScreenRoute> {
-            SplashScreen(
-                onNavigateToWelcomeScreen = {
-                    navController.navigate(WelcomeScreenRoute)
-                }
-            )
-        }
+    KoinContext {
+        NavHost(
+            navController = navController,
+            startDestination = SplashScreenRoute,
+            enterTransition = NavAnimations.slideLeftEnterAnimation,
+            exitTransition = NavAnimations.slideLeftExitAnimation,
+            popEnterTransition = NavAnimations.popEnterRightAnimation,
+            popExitTransition = NavAnimations.popExitRightAnimation
+        ) {
+            composable<SplashScreenRoute> {
+                SplashScreen(
+                    onNavigateToWelcomeScreen = {
+                        navController.navigate(WelcomeScreenRoute)
+                    }
+                )
+            }
 
-        composable<WelcomeScreenRoute> {
-            WelcomeScreen(
-                onNavigateToLogin = {
-                    navController.navigate(LoginScreenRoute)
-                }
-            )
-        }
+            composable<WelcomeScreenRoute> {
+                WelcomeScreen(
+                    onNavigateToLogin = {
+                        navController.navigate(LoginScreenRoute)
+                    }
+                )
+            }
 
-        composable<LoginScreenRoute> {
-            val viewModel: LoginUserViewModel = koinInject()
-            val stateSignInGoogle by viewModel.stateSignInGoogle.collectAsStateWithLifecycle()
-            val coroutineScope = rememberCoroutineScope()
-            val launcher = rememberLauncherForActivityResult(
-                contract = ActivityResultContracts.StartIntentSenderForResult(),
-                onResult = { result ->
-                    if (result.resultCode == Activity.RESULT_OK) {
-                        coroutineScope.launch {
-                            val signInResult = googleAuthUiClient.signInWithIntent(
-                                intent = result.data ?: return@launch
-                            )
-                            viewModel.onGoogleSignInResult(signInResult)
+            composable<LoginScreenRoute> {
+                val viewModel: LoginUserViewModel = koinInject()
+                val stateSignInGoogle by viewModel.stateSignInGoogle.collectAsStateWithLifecycle()
+                val coroutineScope = rememberCoroutineScope()
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartIntentSenderForResult(),
+                    onResult = { result ->
+                        if (result.resultCode == Activity.RESULT_OK) {
+                            coroutineScope.launch {
+                                val signInResult = googleAuthUiClient.signInWithIntent(
+                                    intent = result.data ?: return@launch
+                                )
+                                viewModel.onGoogleSignInResult(signInResult)
+                            }
                         }
                     }
-                }
-            )
+                )
 
-            LaunchedEffect(key1 = Unit) {
-                if (googleAuthUiClient.getSignedInUser() != null) {
-                    Toast.makeText(
-                        context,
-                        "Sign in successful, navigate to Profile",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-
-            LaunchedEffect(key1 = stateSignInGoogle.isSignInSuccessful) {
-                if (stateSignInGoogle.isSignInSuccessful) {
-
-                    val userGoogleData = googleAuthUiClient.getSignedInUser()
-                    viewModel.saveUserGoogleData(userGoogleData)
-
-                    Toast.makeText(
-                        context,
-                        "Sign in successful",
-                        Toast.LENGTH_LONG
-                    ).show()
-                    viewModel.resetState()
-
-                    navController.navigate(SignInScreenRoute)
-                }
-            }
-
-            LoginScreen(
-                onGoogleSignInClick = {
-                    coroutineScope.launch {
-                        val signInIntentSender = googleAuthUiClient.signInGoogle()
-                        launcher.launch(
-                            IntentSenderRequest.Builder(
-                                signInIntentSender ?: return@launch
-                            ).build()
-                        )
+                LaunchedEffect(key1 = Unit) {
+                    if (googleAuthUiClient.getSignedInUser() != null) {
+                        Toast.makeText(
+                            context,
+                            "Sign in successful, navigate to Profile",
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
-                },
-                onNavigateToSignIn = {
-                    navController.navigate(SignInScreenRoute)
-                },
-                onNavigateToHome = {},
-            )
+                }
+
+                LaunchedEffect(key1 = stateSignInGoogle.isSignInSuccessful) {
+                    if (stateSignInGoogle.isSignInSuccessful) {
+
+                        val userGoogleData = googleAuthUiClient.getSignedInUser()
+                        viewModel.saveUserGoogleData(userGoogleData)
+
+                        Toast.makeText(
+                            context,
+                            "Sign in successful",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        viewModel.resetState()
+
+                        navController.navigate(SignInScreenRoute)
+                    }
+                }
+
+                LoginScreen(
+                    onGoogleSignInClick = {
+                        coroutineScope.launch {
+                            val signInIntentSender = googleAuthUiClient.signInGoogle()
+                            launcher.launch(
+                                IntentSenderRequest.Builder(
+                                    signInIntentSender ?: return@launch
+                                ).build()
+                            )
+                        }
+                    },
+                    onNavigateToSignIn = {
+                        navController.navigate(SignInScreenRoute)
+                    },
+                    onNavigateToHome = {},
+                )
 
 
-        }
+            }
 
-        composable<SignInScreenRoute> {
-            SignInScreen(
-                onNavigateToHome = {}
-            )
+            composable<SignInScreenRoute> {
+                SignInScreen(
+                    onNavigateToHome = {}
+                )
+            }
         }
     }
 }
