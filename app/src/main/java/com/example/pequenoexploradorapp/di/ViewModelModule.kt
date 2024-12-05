0d75b9300a7291d5a24057ccb96de4ede87a25e7
walter <walter.d.retzer@gmail.com>
2024-12-05 10:47:06 -0300

    Implementado SharedPrefApp by Koin

--- app/src/main/java/com/example/pequenoexploradorapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/ViewModelModule.kt
@@ -2,17 +2,24 @@ package com.example.pequenoexploradorapp.di
 
 import android.content.Context
 import com.example.pequenoexploradorapp.domain.connectivity.AndroidConnectivityObserver
+import com.example.pequenoexploradorapp.domain.secure.SharedPrefApp
 import com.example.pequenoexploradorapp.presentation.viewmodel.LoginUserViewModel
 import com.example.pequenoexploradorapp.presentation.viewmodel.SignInViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
+
 val viewModelModules = module {
-    single { provideConnectivityStatus(androidApplication())}
-    factory <LoginUserViewModel> { LoginUserViewModel(provideConnectivityStatus(androidApplication())) }
-    factory <SignInViewModel> {  SignInViewModel(get()) }
+    single { provideSharedPref(androidApplication()) }
+    single { provideConnectivityStatus(androidApplication()) }
+    factory<LoginUserViewModel> { LoginUserViewModel(provideConnectivityStatus(androidApplication()), get()) }
+    factory<SignInViewModel> { SignInViewModel(get(), get()) }
 }
 
-fun provideConnectivityStatus(client: Context): AndroidConnectivityObserver {
-    return AndroidConnectivityObserver(client)
+fun provideConnectivityStatus(context: Context): AndroidConnectivityObserver {
+    return AndroidConnectivityObserver(context)
+}
+
+fun provideSharedPref(context: Context): SharedPrefApp {
+    return SharedPrefApp(context)
 }
