6f9da9bb4bd418eb1d6ae9388489a5281c3c86fd
walter <walter.d.retzer@gmail.com>
2024-12-05 11:12:55 -0300

    Incluído kotlinx.serialization.SerialName na dta class RoverMission

--- app/src/main/java/com/example/pequenoexploradorapp/data/DataRoverMission.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/data/DataRoverMission.kt
@@ -1,22 +1,32 @@
 package com.example.pequenoexploradorapp.data
 
+import androidx.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Keep
+@Serializable
 data class RoverMission(
-    val rover: Rover
+    @SerialName("rover") val rover: Rover
 )
 
+@Keep
+@Serializable
 data class Rover(
-    val id: String,
-    val name: String,
-    val landing_date: String,
-    val launch_date: String,
-    val status: String,
-    val max_sol: String,
-    val max_date: String,
-    val total_photos: Int,
-    val cameras: List<RoverCameraType>
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String,
+    @SerialName("landing_date") val landingDate: String,
+    @SerialName("launch_date") val launchDate: String,
+    @SerialName("status") val status: String,
+    @SerialName("max_sol") val maxSun: String,
+    @SerialName("max_date") val maxDate: String,
+    @SerialName("total_photos") val totalPhotos: Int,
+    @SerialName("cameras") val cameras: List<RoverCameraType>
 )
 
+@Keep
+@Serializable
 data class RoverCameraType(
-    val name: String,
-    val full_name: String
+    @SerialName("name") val name: String,
+    @SerialName("full_name") val fullName: String
 )
