bff52cde50b0e0f072efa1e850c20a42e500d969
walter <walter.d.retzer@gmail.com>
2024-12-12 18:41:21 -0300

    Implementado o uiState na tela SearchImageScreen

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/SearchImageScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/SearchImageScreen.kt
@@ -1,5 +1,6 @@
 package com.example.pequenoexploradorapp.presentation.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -15,17 +16,24 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -34,17 +42,28 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.pequenoexploradorapp.R
+import com.example.pequenoexploradorapp.domain.util.ConstantsApp
 import com.example.pequenoexploradorapp.presentation.components.MenuToolbar
 import com.example.pequenoexploradorapp.presentation.components.ProgressButton
 import com.example.pequenoexploradorapp.presentation.components.parallax.ParallaxView
 import com.example.pequenoexploradorapp.presentation.components.parallax.model.ContainerSettings
+import com.example.pequenoexploradorapp.presentation.viewmodel.SearchImageViewModel
+import com.example.pequenoexploradorapp.presentation.viewmodel.SearchImageViewState
+import org.koin.compose.koinInject
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchImageScreen() {
-
+fun SearchImageScreen(
+    viewModel: SearchImageViewModel = koinInject()
+) {
+    val snackBarHostState = remember { SnackbarHostState() }
     val toolbarBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val uiState by viewModel.uiState.collectAsState()
+    val isConnected by viewModel.isConnected.collectAsStateWithLifecycle()
+    val textSearchImage by viewModel.searchImageState.collectAsState()
+    var progressButtonIsActivated by remember { mutableStateOf(false) }
 
     ParallaxView(
         backgroundContent = {
@@ -58,6 +77,7 @@ fun SearchImageScreen() {
         },
         middleContent = {
             Scaffold(
+                snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
                 topBar = {
                     MenuToolbar(
                         title = "Menu",
@@ -70,6 +90,28 @@ fun SearchImageScreen() {
                 },
                 containerColor = Color.Transparent
             ) { paddingValues ->
+                when (val state = uiState) {
+                    is SearchImageViewState.DrawScreen -> {}
+                    is SearchImageViewState.Loading -> {
+                        progressButtonIsActivated = true
+                    }
+                    is SearchImageViewState.Error -> {
+                        progressButtonIsActivated = false
+                        Toast.makeText(LocalContext.current, state.message, Toast.LENGTH_LONG)
+                            .show()
+                    }
+                    is SearchImageViewState.Success -> {
+                        progressButtonIsActivated = false
+                        Toast.makeText(LocalContext.current, "Sucesso", Toast.LENGTH_LONG).show()
+                    }
+                }
+                if (isConnected?.not() == true) {
+                    Toast.makeText(
+                        LocalContext.current,
+                        ConstantsApp.ERROR_WITHOUT_INTERNET,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -91,13 +133,13 @@ fun SearchImageScreen() {
                             fontSize = 18.sp,
                             fontWeight = FontWeight.Normal,
                             textAlign = TextAlign.Justify,
-                            color = MaterialTheme.colorScheme.secondary
+                            color = Color.White
                         )
                         Spacer(modifier = Modifier.height(6.dp))
                         OutlinedTextField(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .background(Color.Transparent),
+                                .background(Color.DarkGray, RoundedCornerShape(20.dp)),
                             keyboardOptions = KeyboardOptions(
                                 capitalization = KeyboardCapitalization.None,
                                 autoCorrectEnabled = true,
@@ -105,10 +147,10 @@ fun SearchImageScreen() {
                                 imeAction = ImeAction.Next
                             ),
                             shape = RoundedCornerShape(20.dp),
-                            value = "Saturno",
+                            value = textSearchImage.textInput,
                             isError = false,
                             placeholder = { Text("Procurar Imagens") },
-                            onValueChange = { }
+                            onValueChange = { viewModel.onTextInputChange(it) }
                         )
                         Spacer(modifier = Modifier.height(10.dp))
                         ProgressButton(
@@ -116,8 +158,8 @@ fun SearchImageScreen() {
                                 .padding(top = 16.dp)
                                 .fillMaxWidth(),
                             text = "Pesquisar",
-                            isLoading = false,
-                            onClick = { }
+                            isLoading = progressButtonIsActivated,
+                            onClick = { viewModel.onNasaImageSearch(textSearchImage.textInput) }
                         )
                     }
                 }
