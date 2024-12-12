7edf7bff4219803772957f7c145415755e2be9dd
walter <walter.d.retzer@gmail.com>
2024-12-12 18:35:40 -0300

    Incluído parametro messageError

--- app/src/main/java/com/example/pequenoexploradorapp/domain/network/ApiResponse.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/network/ApiResponse.kt
@@ -3,11 +3,18 @@ package com.example.pequenoexploradorapp.domain.network
 
 sealed class ApiResponse<out T> {
     data class Success<out T>(val data: T, val statusCode: Int) : ApiResponse<T>()
-    data class Failure(val exception: Throwable, val statusCode: Int?) : ApiResponse<Nothing>()
+    data class Failure(
+        val exception: Throwable,
+        val statusCode: Int?,
+        val messageError: String
+    ) : ApiResponse<Nothing>()
 
     companion object {
         fun <T> success(data: T, statusCode: Int): ApiResponse<T> = Success(data, statusCode)
-        fun failure(exception: Throwable, statusCode: Int?): ApiResponse<Nothing> =
-            Failure(exception, statusCode)
+        fun failure(
+            exception: Throwable,
+            statusCode: Int?,
+            messageError: String
+        ): ApiResponse<Nothing> = Failure(exception, statusCode, messageError)
     }
 }
