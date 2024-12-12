77f5981b9680060cb5dd420a80463daae366e64b
walter <walter.d.retzer@gmail.com>
2024-12-12 18:36:40 -0300

    Implementado fun getNasaImage

--- app/src/main/java/com/example/pequenoexploradorapp/domain/repository/RemoteRepository.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/repository/RemoteRepository.kt
@@ -1,8 +1,15 @@
 package com.example.pequenoexploradorapp.domain.repository
 
+import com.example.pequenoexploradorapp.data.NasaImageResponse
 import com.example.pequenoexploradorapp.data.RoverMission
 import com.example.pequenoexploradorapp.domain.network.ApiResponse
 
 interface RemoteRepository {
     suspend fun getInfoRoversMission(): ApiResponse<RoverMission>
+
+    suspend fun getNasaImage(
+        imageSearch: String,
+        page: Int = 1,
+        mediaType: String = "image"
+    ): ApiResponse<NasaImageResponse>
 }
