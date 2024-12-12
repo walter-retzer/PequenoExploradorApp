d3e0b25beabe77e99a87a648f71c154f95c5e285
walter <walter.d.retzer@gmail.com>
2024-12-12 18:38:38 -0300

    Implementado NasaImageResponse

---
+++ app/src/main/java/com/example/pequenoexploradorapp/data/NasaImageResponse.kt
@@ -0,0 +1,50 @@
+package com.example.pequenoexploradorapp.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class NasaImageResponse(
+    @SerialName("collection") val collection: NasaImageCollection
+)
+
+@Serializable
+data class NasaImageCollection(
+    @SerialName("href") val href: String?,
+    @SerialName("version") val version: String?,
+    @SerialName("items") val items: List<NasaImageItems>? = null,
+    @SerialName("links") val links: List<NasaImageNextPage>? = null,
+    @SerialName("metadata") val metadata: NasaImageMetaData?
+)
+
+@Serializable
+data class NasaImageItems(
+    @SerialName("href") val href: String?,
+    @SerialName("data") val data: List<NasaImageData?>,
+    @SerialName("links") val links: List<NasaImageLink?>,
+    @SerialName("isFavourite") val isFavourite: Boolean = false
+)
+
+@Serializable
+data class NasaImageData(
+    @SerialName("title") var title: String?,
+    @SerialName("date_created") val dateCreated: String?,
+    @SerialName("secondary_creator") val creators: String? = null,
+    @SerialName("keywords") val keywords: List<String>? = null
+)
+
+@Serializable
+data class NasaImageLink(
+    @SerialName("href") val href: String?
+)
+
+@Serializable
+data class NasaImageNextPage(
+    @SerialName("href") val nextPage: String?
+)
+
+@Serializable
+data class NasaImageMetaData(
+    @SerialName("total_hits") val totalHits: Int = 0
+)
