8adb98b5de7eb2b395338b125c6875c798aa59e9
walter <walter.d.retzer@gmail.com>
2024-12-12 21:37:25 -0300

    Ajuste da exibição do ParallaxView na tela de SearchImageScreen

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/SearchImageScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/SearchImageScreen.kt
@@ -3,12 +3,8 @@ package com.example.pequenoexploradorapp.presentation.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -33,6 +29,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -69,148 +66,147 @@ fun SearchImageScreen(
     var progressButtonIsActivated by remember { mutableStateOf(false) }
     var snackBarIsActivated by remember { mutableStateOf(false) }
 
-    ParallaxView(
-        backgroundContent = {
-            Image(
-                painter = painterResource(id = R.drawable.bg_galaxy),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentScale = ContentScale.FillHeight
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+        topBar = {
+            MenuToolbar(
+                title = "Imagens",
+                onNavigationToMenu = { },
+                onNavigationToProfile = { },
+                onNavigateToNotifications = { },
+                toolbarBehavior = toolbarBehavior,
+                isActivatedBadge = false
             )
         },
-        middleContent = {
-            Scaffold(
-                snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
-                topBar = {
-                    MenuToolbar(
-                        title = "Menu",
-                        onNavigationToMenu = { },
-                        onNavigationToProfile = { },
-                        onNavigateToNotifications = { },
-                        toolbarBehavior = toolbarBehavior,
-                        isActivatedBadge = false
+        containerColor = Color.Transparent
+    ) { paddingValues ->
+        when (val state = uiState) {
+            is SearchImageViewState.DrawScreen -> {}
+
+            is SearchImageViewState.Loading -> {
+                progressButtonIsActivated = true
+            }
+
+            is SearchImageViewState.Error -> {
+                progressButtonIsActivated = false
+                snackBarIsActivated = true
+                LaunchedEffect(snackBarIsActivated) {
+                    snackBarOnlyMessage(
+                        snackBarHostState = snackBarHostState,
+                        coroutineScope = scope,
+                        message = state.message,
+                        duration = SnackbarDuration.Long
                     )
-                },
-                containerColor = Color.Transparent
-            ) { paddingValues ->
-                when (val state = uiState) {
-                    is SearchImageViewState.DrawScreen -> {}
-                    is SearchImageViewState.Loading -> {
-                        progressButtonIsActivated = true
-                    }
-                    is SearchImageViewState.Error -> {
-                        progressButtonIsActivated = false
-                        snackBarIsActivated = true
-                        LaunchedEffect(snackBarIsActivated) {
-                            snackBarOnlyMessage(
-                                snackBarHostState = snackBarHostState,
-                                coroutineScope = scope,
-                                message = state.message,
-                                duration = SnackbarDuration.Long
-                            )
-                            snackBarIsActivated = false
-                        }
-                    }
-                    is SearchImageViewState.Success -> {
-                        progressButtonIsActivated = false
-                        snackBarIsActivated = true
-                        LaunchedEffect(snackBarIsActivated) {
-                            snackBarOnlyMessage(
-                                snackBarHostState = snackBarHostState,
-                                coroutineScope = scope,
-                                message = ConstantsApp.SUCCESS_SIGN_IN,
-                                duration = SnackbarDuration.Long
-                            )
-                            snackBarIsActivated = false
-                        }
-                    }
-                }
-                if (isConnected?.not() == true) {
-                    snackBarIsActivated = true
-                    LaunchedEffect(snackBarIsActivated) {
-                        snackBarOnlyMessage(
-                            snackBarHostState = snackBarHostState,
-                            coroutineScope = scope,
-                            message = ConstantsApp.ERROR_WITHOUT_INTERNET,
-                            duration = SnackbarDuration.Long
-                        )
-                        snackBarIsActivated = false
-                    }
-                }
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(paddingValues),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(start = 16.dp, end = 16.dp)
-                            .fillMaxWidth()
-                            .verticalScroll(rememberScrollState()),
-                        verticalArrangement = Arrangement.Top,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Spacer(modifier = Modifier.height(6.dp))
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Encontre as imagens mais facinantes do Universo",
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Normal,
-                            textAlign = TextAlign.Justify,
-                            color = Color.White
-                        )
-                        Spacer(modifier = Modifier.height(6.dp))
-                        OutlinedTextField(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(Color.DarkGray, RoundedCornerShape(20.dp)),
-                            keyboardOptions = KeyboardOptions(
-                                capitalization = KeyboardCapitalization.None,
-                                autoCorrectEnabled = true,
-                                keyboardType = KeyboardType.Text,
-                                imeAction = ImeAction.Next
-                            ),
-                            shape = RoundedCornerShape(20.dp),
-                            value = textSearchImage.textInput,
-                            isError = false,
-                            placeholder = { Text("Procurar Imagens") },
-                            onValueChange = { viewModel.onTextInputChange(it) }
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                        ProgressButton(
-                            modifier = Modifier
-                                .padding(top = 16.dp)
-                                .fillMaxWidth(),
-                            text = "Pesquisar",
-                            isLoading = progressButtonIsActivated,
-                            onClick = { viewModel.onNasaImageSearch(textSearchImage.textInput) }
-                        )
-                    }
+                    snackBarIsActivated = false
                 }
             }
-        },
-        foregroundContent = {
-            Image(
-                painter = painterResource(id = R.drawable.fg_astronaut_night),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.fillMaxWidth()
+
+            is SearchImageViewState.Success -> {
+                progressButtonIsActivated = false
+                snackBarIsActivated = true
+                LaunchedEffect(snackBarIsActivated) {
+                    snackBarOnlyMessage(
+                        snackBarHostState = snackBarHostState,
+                        coroutineScope = scope,
+                        message = ConstantsApp.SUCCESS_SIGN_IN,
+                        duration = SnackbarDuration.Long
+                    )
+                    snackBarIsActivated = false
+                }
+            }
+        }
+        if (isConnected?.not() == true) {
+            snackBarIsActivated = true
+            LaunchedEffect(snackBarIsActivated) {
+                snackBarOnlyMessage(
+                    snackBarHostState = snackBarHostState,
+                    coroutineScope = scope,
+                    message = ConstantsApp.ERROR_WITHOUT_INTERNET,
+                    duration = SnackbarDuration.Long
+                )
+                snackBarIsActivated = false
+            }
+        }
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .paint(
+                    painterResource(id = R.drawable.simple_background),
+                    contentScale = ContentScale.FillBounds
+                )
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ParallaxView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black),
+                backgroundContent = {
+                    Image(
+                        painter = painterResource(id = R.drawable.bg_galaxy),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentScale = ContentScale.FillWidth
+                    )
+                },
+                middleContent = { },
+                foregroundContent = {
+                    Image(
+                        painter = painterResource(id = R.drawable.fg_astronaut_night),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                },
+                backgroundContainerSettings = ContainerSettings(
+                    scale = 1.5f,
+                ),
+                foregroundContainerSettings = ContainerSettings(
+                    scale = 1.5f,
+                    alignment = Alignment.BottomCenter
+                ),
+                verticalOffsetLimit = 0.12f,
+                horizontalOffsetLimit = 0.5f,
+                movementIntensityMultiplier = 40,
             )
-        },
-        backgroundContainerSettings = ContainerSettings(
-            scale = 1.2f,
-        ),
-        foregroundContainerSettings = ContainerSettings(
-            scale = 1.5f,
-            alignment = Alignment.BottomCenter
-        ),
-        verticalOffsetLimit = 0.12f,
-        horizontalOffsetLimit = 0.5f,
-        movementIntensityMultiplier = 40,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Black)
-    )
+            Text(
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 10.dp)
+                    .fillMaxWidth(),
+                text = "Encontre as imagens mais facinantes do Universo",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Justify,
+                color = Color.White
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp)
+                    .fillMaxWidth()
+                    .background(Color.DarkGray, RoundedCornerShape(20.dp)),
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.None,
+                    autoCorrectEnabled = true,
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
+                shape = RoundedCornerShape(20.dp),
+                value = textSearchImage.textInput,
+                isError = false,
+                placeholder = { Text("Procurar Imagens") },
+                onValueChange = { viewModel.onTextInputChange(it) }
+            )
+            ProgressButton(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                text = "Pesquisar",
+                isLoading = progressButtonIsActivated,
+                onClick = { viewModel.onNasaImageSearch(textSearchImage.textInput) }
+            )
+        }
+    }
 }
