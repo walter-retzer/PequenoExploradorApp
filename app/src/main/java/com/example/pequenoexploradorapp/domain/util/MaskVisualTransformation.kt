88a76c9543bb23f343a91d831614bfe6a1456759
walter <walter.d.retzer@gmail.com>
2025-01-10 15:34:37 -0300

    Implementado fun para correção do prefix da url das imagens dos rovers

--- app/src/main/java/com/example/pequenoexploradorapp/domain/util/MaskVisualTransformation.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/util/MaskVisualTransformation.kt
@@ -92,3 +92,12 @@ fun Long.toBrazilianDateFormat(
     }
     return formatter.format(date)
 }
+
+
+fun String.toHttpsPrefix(): String =
+    if (isNotEmpty() && !startsWith("https://") && !startsWith("http://")) {
+        "https://$this"
+    } else if (startsWith("http://")) {
+        replace("http://", "https://")
+    } else this
+
