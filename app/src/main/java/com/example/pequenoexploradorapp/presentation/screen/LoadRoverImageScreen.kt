fdc8e99e8e002777854a500fa656691d70999f21
walter <walter.d.retzer@gmail.com>
2025-01-10 15:29:28 -0300

    Tela inicial do LoadRoverImageScreen

---
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/LoadRoverImageScreen.kt
@@ -0,0 +1,377 @@
+package com.example.pequenoexploradorapp.presentation.screen
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItemDefaults.contentColor
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import com.example.pequenoexploradorapp.R
+import com.example.pequenoexploradorapp.data.RoverImageInfo
+import com.example.pequenoexploradorapp.domain.util.ConstantsApp
+import com.example.pequenoexploradorapp.domain.util.snackBarOnlyMessage
+import com.example.pequenoexploradorapp.domain.util.toHttpsPrefix
+import com.example.pequenoexploradorapp.presentation.components.AnimatedLottieFile
+import com.example.pequenoexploradorapp.presentation.components.MenuToolbar
+import com.example.pequenoexploradorapp.presentation.theme.mainColor
+import com.example.pequenoexploradorapp.presentation.viewmodel.LoadRoverImageViewModel
+import com.example.pequenoexploradorapp.presentation.viewmodel.LoadRoverImageViewState
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoadRoverImageScreen(
+    date: String?,
+    viewModel: LoadRoverImageViewModel = koinInject()
+) {
+    val scrollState = rememberLazyGridState()
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val toolbarBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val uiState by viewModel.uiState.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val isConnected by viewModel.isConnected.collectAsStateWithLifecycle()
+    var progressButtonIsActivated by remember { mutableStateOf(false) }
+    var snackBarIsActivated by remember { mutableStateOf(false) }
+    var listOfRoverImages = listOf<RoverImageInfo>()
+    var page by remember { mutableIntStateOf(1) }
+
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+        topBar = {
+            MenuToolbar(
+                title = "Imagens",
+                onNavigationToMenu = { },
+                onNavigationToProfile = { },
+                onNavigateToNotifications = { },
+                toolbarBehavior = toolbarBehavior,
+                isActivatedBadge = false
+            )
+        },
+        containerColor = Color.Transparent
+    ) { paddingValues ->
+        when (val state = uiState) {
+            is LoadRoverImageViewState.Init -> {
+                viewModel.onRoverSpiritImages()
+            }
+
+            is LoadRoverImageViewState.Loading -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .paint(
+                            painterResource(id = R.drawable.simple_background),
+                            contentScale = ContentScale.FillBounds
+                        )
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data("http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01004/opgs/edr/fcam/FLB_486615455EDR_F0481570FHAZ00323M_.JPG")
+                            .crossfade(true)
+                            .build(),
+                        placeholder = painterResource(R.drawable.simple_background),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp)
+                    )
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .width(64.dp)
+                            .align(Alignment.Center),
+                        color = mainColor
+                    )
+                }
+            }
+
+            is LoadRoverImageViewState.Error -> {
+                progressButtonIsActivated = false
+                snackBarIsActivated = true
+                LaunchedEffect(snackBarIsActivated) {
+                    snackBarOnlyMessage(
+                        snackBarHostState = snackBarHostState,
+                        coroutineScope = scope,
+                        message = state.message,
+                        duration = SnackbarDuration.Long
+                    )
+                    snackBarIsActivated = false
+                }
+            }
+
+            is LoadRoverImageViewState.Success -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .paint(
+                            painterResource(id = R.drawable.simple_background),
+                            contentScale = ContentScale.FillBounds
+                        ),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    if (state.images.photos.isEmpty()) {
+                        Box {
+                            AnimatedLottieFile(
+                                modifier = Modifier
+                                    .padding(top = 20.dp)
+                                    .size(300.dp)
+                                    .align(Alignment.TopCenter),
+                                file = R.raw.animation_telescopy
+                            )
+                        }
+                        Row {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                text = "Infelizmente, não foi possível encontrar as imagens pesquizadas",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Normal,
+                                textAlign = TextAlign.Justify,
+                                color = Color.White
+                            )
+                        }
+                    } else {
+                        Row {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                                    .clickable {
+                                        scope.launch {
+                                            scrollState.animateScrollToItem(0)
+                                        }
+                                    },
+                                text = "Foram encontradas ${state.images.photos.size} imagens",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Normal,
+                                textAlign = TextAlign.Justify,
+                                color = Color.White
+                            )
+                        }
+                    }
+                    Row {
+
+                        LazyVerticalGrid(
+                            state = scrollState,
+                            contentPadding = PaddingValues(all = 8.dp),
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            columns = GridCells.Fixed(2),
+                            modifier = Modifier.clipToBounds(),
+                        ) {
+                            state.images.photos.let { imagesToLoad ->
+
+                                items(imagesToLoad.size) { numberOfImage ->
+                                    LoadRoverImageOnCard(
+                                        images = imagesToLoad,
+                                        numberOfImage = numberOfImage,
+                                    )
+                                }
+                            }
+
+                        }
+                        InfiniteImageListHandler(
+                            listState = scrollState,
+                            onLoadMore = {
+                                page++
+                                viewModel.loadNextItems(
+                                    date = "",
+                                    page = page
+                                )
+                            }
+                        )
+                    }
+                }
+                AnimatedVisibility(
+                    visible = isLoading,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Transparent)
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .width(64.dp)
+                                .align(Alignment.Center),
+                            color = mainColor
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    if (isConnected?.not() == true) {
+        snackBarIsActivated = true
+        LaunchedEffect(snackBarIsActivated) {
+            snackBarOnlyMessage(
+                snackBarHostState = snackBarHostState,
+                coroutineScope = scope,
+                message = ConstantsApp.ERROR_WITHOUT_INTERNET,
+                duration = SnackbarDuration.Long
+            )
+            snackBarIsActivated = false
+        }
+    }
+}
+
+
+@Composable
+fun InfiniteImageListHandler(
+    listState: LazyGridState,
+    buffer: Int = 10,
+    onLoadMore: () -> Unit
+) {
+    val shouldLoadMore = remember {
+        derivedStateOf {
+            val totalItemsCount = listState.layoutInfo.totalItemsCount
+            val lastVisibleItemIndex =
+                listState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0
+            lastVisibleItemIndex >= (totalItemsCount - buffer)
+        }
+    }
+
+    LaunchedEffect(shouldLoadMore) {
+        snapshotFlow { shouldLoadMore.value }
+            .distinctUntilChanged()
+            .filter { it }
+            .collect {
+                onLoadMore()
+            }
+    }
+}
+
+@Composable
+fun LoadRoverImageOnCard(images: List<RoverImageInfo>, numberOfImage: Int) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val imageToLoad = images[numberOfImage].image.toHttpsPrefix()
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.Black)
+                .border(
+                    width = 1.dp,
+                    color = contentColor,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .clickable { },
+            elevation = CardDefaults.cardElevation(8.dp)
+        ) {
+
+            SubcomposeAsyncImage(
+                model = imageToLoad,
+                loading = {
+                    Box(contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .size(24.dp)
+                                .align(Alignment.Center),
+                            color = mainColor
+                        )
+                    }
+                },
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+            )
+
+//            AsyncImage(
+//                model = ImageRequest.Builder(LocalContext.current)
+//                    .data(imageToLoad)
+//                    .crossfade(true)
+//                    .build(),
+//                placeholder = painterResource(R.drawable.simple_background),
+//                contentDescription = "",
+//                contentScale = ContentScale.Crop,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(150.dp)
+//            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 6.dp),
+                text = "Imagem: ${numberOfImage + 1}",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Justify,
+                color = contentColor
+            )
+        }
+    }
+}
