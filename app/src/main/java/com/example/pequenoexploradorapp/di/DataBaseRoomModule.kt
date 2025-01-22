c87a2de2ffbe87ddd94cdcf109172e3df9e7852a
walter <walter.d.retzer@gmail.com>
2025-01-22 18:09:53 -0300

    Implementado NasaImageRepository ao DI

--- app/src/main/java/com/example/pequenoexploradorapp/di/DataBaseRoomModule.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/DataBaseRoomModule.kt
@@ -2,11 +2,14 @@ package com.example.pequenoexploradorapp.di
 
 import androidx.room.Room
 import com.example.pequenoexploradorapp.domain.database.NasaImagesDatabase
+import com.example.pequenoexploradorapp.domain.repository.NasaImageRepository
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 
 val dataBaseRoomModules = module {
+    singleOf(::NasaImageRepository)
     single {
         Room.databaseBuilder(
             androidContext(),
