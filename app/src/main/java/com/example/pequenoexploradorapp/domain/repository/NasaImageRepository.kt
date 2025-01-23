916164e17464707c8ae771eddbc98daa3a362247
walter <walter.d.retzer@gmail.com>
2025-01-23 13:13:00 -0300

    Ajuste da fun findAll no repository

--- app/src/main/java/com/example/pequenoexploradorapp/domain/repository/NasaImageRepository.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/repository/NasaImageRepository.kt
@@ -1,18 +1,19 @@
 package com.example.pequenoexploradorapp.domain.repository
 
 import com.example.pequenoexploradorapp.data.NasaImageData
-import com.example.pequenoexploradorapp.data.NasaImageItems
 import com.example.pequenoexploradorapp.domain.database.NasaImageDao
 import com.example.pequenoexploradorapp.domain.database.NasaImageEntity
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.SerialName
 
 class NasaImageRepository(
     private val dao: NasaImageDao
 ) {
 
-    val images get() = dao.findAll()
+    suspend fun getFavouriteImage(): List<NasaImageData> {
+        val response = dao.findAll().map { it.toNasaImage() }
+        return response
+    }
 
     suspend fun save(images: NasaImageData) = withContext(IO) {
         dao.save(images.toNasaImageEntity())
@@ -20,6 +21,13 @@ class NasaImageRepository(
 }
 
 fun NasaImageData.toNasaImageEntity() = NasaImageEntity(
+    title = this.title,
+    dateCreated = this.dateCreated,
+    creators = this.creators,
+    isFavourite = this.isFavourite
+)
+
+fun NasaImageEntity.toNasaImage() = NasaImageData(
     title = this.title,
     dateCreated = this.dateCreated,
     creators = this.creators,
