36ce1fe87e6daf4af44ccc8103c947036f8cf296
walter <walter.d.retzer@gmail.com>
2025-01-23 13:10:02 -0300

    Implementado tela inicial das imagens favoritas

---
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/LoadFavouriteImageScreen.kt
@@ -0,0 +1,195 @@
+package com.example.pequenoexploradorapp.presentation.screen
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItemDefaults.contentColor
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.SubcomposeAsyncImage
+import com.example.pequenoexploradorapp.R
+import com.example.pequenoexploradorapp.data.NasaImageItems
+import com.example.pequenoexploradorapp.domain.util.ConstantsApp
+import com.example.pequenoexploradorapp.domain.util.formattedDate
+import com.example.pequenoexploradorapp.domain.util.snackBarOnlyMessage
+import com.example.pequenoexploradorapp.domain.util.toHttpsPrefix
+import com.example.pequenoexploradorapp.presentation.components.AnimatedLottieFile
+import com.example.pequenoexploradorapp.presentation.components.MenuToolbar
+import com.example.pequenoexploradorapp.presentation.theme.mainColor
+import com.example.pequenoexploradorapp.presentation.theme.primaryLight
+import com.example.pequenoexploradorapp.presentation.theme.secondaryLight
+import com.example.pequenoexploradorapp.presentation.theme.surfaceDark
+import com.example.pequenoexploradorapp.presentation.viewmodel.LoadFavouriteImageViewModel
+import com.example.pequenoexploradorapp.presentation.viewmodel.LoadFavouriteImageViewState
+import com.example.pequenoexploradorapp.presentation.viewmodel.LoadNasaImageViewModel
+import com.example.pequenoexploradorapp.presentation.viewmodel.LoadNasaImageViewState
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoadFavouriteImageScreen(
+    viewModel: LoadFavouriteImageViewModel = koinInject()
+) {
+    val scrollState = rememberLazyGridState()
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val toolbarBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val uiState by viewModel.uiState.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val isConnected by viewModel.isConnected.collectAsStateWithLifecycle()
+    var progressButtonIsActivated by remember { mutableStateOf(false) }
+    var snackBarIsActivated by remember { mutableStateOf(false) }
+
+
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+        topBar = {
+            MenuToolbar(
+                title = "Imagens",
+                onNavigationToMenu = { },
+                onNavigationToProfile = { },
+                onNavigateToNotifications = { },
+                toolbarBehavior = toolbarBehavior,
+                isActivatedBadge = false
+            )
+        },
+        containerColor = Color.Transparent
+    ) { paddingValues ->
+        when (val state = uiState) {
+            is LoadFavouriteImageViewState.Init -> {
+                viewModel.onLoadListFavouriteImage()
+            }
+
+            is LoadFavouriteImageViewState.Loading -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .paint(
+                            painterResource(id = R.drawable.simple_background),
+                            contentScale = ContentScale.FillBounds
+                        )
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .width(64.dp)
+                            .align(Alignment.Center),
+                        color = mainColor
+                    )
+                }
+            }
+
+            is LoadFavouriteImageViewState.Error -> {
+                progressButtonIsActivated = false
+                snackBarIsActivated = true
+                LaunchedEffect(snackBarIsActivated) {
+                    snackBarOnlyMessage(
+                        snackBarHostState = snackBarHostState,
+                        coroutineScope = scope,
+                        message = state.message,
+                        duration = SnackbarDuration.Long
+                    )
+                    snackBarIsActivated = false
+                }
+            }
+
+            is LoadFavouriteImageViewState.Success -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .paint(
+                            painterResource(id = R.drawable.simple_background),
+                            contentScale = ContentScale.FillBounds
+                        ),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = state.images.toString(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Center,
+                        color = contentColor
+                    )
+                }
+            }
+        }
+
+        if (isConnected?.not() == true) {
+            snackBarIsActivated = true
+            LaunchedEffect(snackBarIsActivated) {
+                snackBarOnlyMessage(
+                    snackBarHostState = snackBarHostState,
+                    coroutineScope = scope,
+                    message = ConstantsApp.ERROR_WITHOUT_INTERNET,
+                    duration = SnackbarDuration.Long
+                )
+                snackBarIsActivated = false
+            }
+        }
+    }
+}
