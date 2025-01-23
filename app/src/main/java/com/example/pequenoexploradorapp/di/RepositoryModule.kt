52a3debfda41f73adab716cc95f8d168b9a94187
walter <walter.d.retzer@gmail.com>
2025-01-23 14:58:55 -0300

    Ajuste do repositorio das imagens favoritas

--- app/src/main/java/com/example/pequenoexploradorapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/RepositoryModule.kt
@@ -1,13 +1,20 @@
 package com.example.pequenoexploradorapp.di
 
-import com.example.pequenoexploradorapp.domain.repository.RemoteRepositoryImpl
+import com.example.pequenoexploradorapp.domain.database.NasaImageDao
+import com.example.pequenoexploradorapp.domain.repository.local.FavouriteImageRepositoryImpl
+import com.example.pequenoexploradorapp.domain.repository.remote.RemoteRepositoryImpl
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single { provideRemoteRepository(get()) }
+    single { provideLocalFavouriteImageRepository(get()) }
 }
 
 fun provideRemoteRepository(client: HttpClient): RemoteRepositoryImpl {
     return RemoteRepositoryImpl(client)
+}
+
+fun provideLocalFavouriteImageRepository(dao: NasaImageDao): FavouriteImageRepositoryImpl {
+    return FavouriteImageRepositoryImpl(dao)
 }
