760e63bfddac6357ad4c049f670bda112fc42a88
walter <walter.d.retzer@gmail.com>
2025-01-23 12:12:33 -0300

    Adicionado Class TypeConvertes ao NasaImagesDatabase

--- app/src/main/java/com/example/pequenoexploradorapp/domain/database/NasaImagesDatabase.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/database/NasaImagesDatabase.kt
@@ -2,10 +2,11 @@ package com.example.pequenoexploradorapp.domain.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.pequenoexploradorapp.domain.database.typeConverter.Converters
 
-@Database(entities = [NasaImageEntity::class], version = 1)
+@Database(entities = [NasaImageEntity::class], version = 2001)
+@TypeConverters(Converters::class)
 abstract class NasaImagesDatabase : RoomDatabase() {
-
     abstract fun taskDao(): NasaImageDao
-
 }
