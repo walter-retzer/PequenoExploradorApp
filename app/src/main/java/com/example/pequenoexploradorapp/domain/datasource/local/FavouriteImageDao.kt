5bf23ccb76630e63cba7c53cd4421f6fd9688a19
walter <walter.d.retzer@gmail.com>
2025-01-24 12:42:37 -0300

    Adicionado métodos de delete

--- app/src/main/java/com/example/pequenoexploradorapp/domain/datasource/local/FavouriteImageDao.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/datasource/local/FavouriteImageDao.kt
@@ -1,17 +1,28 @@
 package com.example.pequenoexploradorapp.domain.datasource.local
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavouriteImageDao {
 
-    @Query("SELECT * FROM FavouriteImageEntity ORDER BY id DESC")
+    @Query("SELECT * FROM favourites ORDER BY id DESC")
     suspend fun findAll(): List<FavouriteImageEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun save(task: FavouriteImageEntity)
+    suspend fun save(favourite: FavouriteImageEntity)
+
+    @Query("SELECT * FROM favourites WHERE id = :id")
+    fun findById(id: String): Flow<FavouriteImageEntity?>
+
+    @Delete
+    suspend fun deleteImageFavourite(favourite: FavouriteImageEntity)
+
+    @Query("DELETE FROM favourites")
+    suspend fun deleteAll()
 
 }
