7be8e8f262dd4538c6e1118a154a0ed64c776fda
walter <walter.d.retzer@gmail.com>
2025-01-25 16:14:51 -0300

    Adicionado default null para as variaveis

--- app/src/main/java/com/example/pequenoexploradorapp/data/PictureOfTheDay.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/data/PictureOfTheDay.kt
@@ -5,12 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PictureOfTheDay(
-    @SerialName("copyright") val copyright: String,
-    @SerialName("date") val date: String,
-    @SerialName("explanation") val explanation: String,
-    @SerialName("hdurl") val hdUrl: String,
-    @SerialName("media_type") val mediaType: String,
-    @SerialName("service_version") val serviceVersion: String,
-    @SerialName("title") val title: String,
-    @SerialName("url") val url: String
+    @SerialName("copyright") val copyright: String? = null,
+    @SerialName("date") val date: String? = null,
+    @SerialName("explanation") val explanation: String? = null,
+    @SerialName("hdurl") val hdUrl: String? = null,
+    @SerialName("media_type") val mediaType: String? = null,
+    @SerialName("service_version") val serviceVersion: String? = null,
+    @SerialName("title") val title: String? = null,
+    @SerialName("url") val url: String? = null
 )
