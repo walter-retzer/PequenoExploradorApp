52ee7baee7c1c86bdd19bed1b0b96d9071d50493
walter <walter.d.retzer@gmail.com>
2025-01-25 17:26:15 -0300

    Adicionado fun para adicionar a imagem do dia aos favoritos.

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/viewmodel/PictureOfTheDayViewModel.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/viewmodel/PictureOfTheDayViewModel.kt
@@ -2,9 +2,12 @@ package com.example.pequenoexploradorapp.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pequenoexploradorapp.data.FavouriteImageToSave
+import com.example.pequenoexploradorapp.data.NasaImageItems
 import com.example.pequenoexploradorapp.data.PictureOfTheDay
 import com.example.pequenoexploradorapp.domain.connectivity.ConnectivityObserver
 import com.example.pequenoexploradorapp.domain.network.ApiResponse
+import com.example.pequenoexploradorapp.domain.repository.local.FavouriteImageRepositoryImpl
 import com.example.pequenoexploradorapp.domain.repository.remote.RemoteRepositoryImpl
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
@@ -21,7 +24,8 @@ import kotlinx.coroutines.launch
 
 class PictureOfTheDayViewModel(
     private val connectivityObserver: ConnectivityObserver,
-    private val remoteRepositoryImpl: RemoteRepositoryImpl
+    private val remoteRepositoryImpl: RemoteRepositoryImpl,
+    private val localRepositoryImpl: FavouriteImageRepositoryImpl
 ) : ViewModel() {
 
     val isConnected = connectivityObserver
@@ -35,6 +39,35 @@ class PictureOfTheDayViewModel(
     private val _uiState = MutableStateFlow<PictureOfTheDayViewState>(PictureOfTheDayViewState.Init)
     val uiState: StateFlow<PictureOfTheDayViewState> = _uiState.asStateFlow()
 
+    private val _isLoading = MutableStateFlow((false))
+    val isLoading = _isLoading.asStateFlow()
+
+    fun onSaveFavourite(
+        imageFavouriteToSave: FavouriteImageToSave,
+        image: PictureOfTheDay
+    ) {
+        viewModelScope.launch {
+            if(checkIfFavouriteExist(imageFavouriteToSave)) (return@launch)
+            _isLoading.value = true
+            localRepositoryImpl.saveImage(imageFavouriteToSave)
+            delay(800L)
+            _isLoading.value = false
+            _uiState.value = PictureOfTheDayViewState.Success(image)
+        }
+    }
+
+    private suspend fun checkIfFavouriteExist(listOfImagesFavourite: FavouriteImageToSave): Boolean {
+        val favouriteImages = localRepositoryImpl.getFavouriteImage()
+        return favouriteImages.any { it.link == listOfImagesFavourite.link }
+    }
+
+    private suspend fun updateFavouriteStatus(listOfImagesFromApi: List<NasaImageItems>): List<NasaImageItems> {
+        val favouriteImages = localRepositoryImpl.getFavouriteImage()
+        return listOfImagesFromApi.map { image ->
+            val isFavourite = favouriteImages.any { it.link == image.links.firstOrNull()?.href }
+            image.copy(isFavourite = isFavourite)
+        }
+    }
 
     fun onPictureOfTheDayRequest() {
         _uiState.value = PictureOfTheDayViewState.Loading
@@ -60,16 +93,25 @@ class PictureOfTheDayViewModel(
                         .addOnFailureListener { exception ->
                             println("Error Download Model Translation: ${exception.printStackTrace()}")
                         }
-                    textTranslator.translate(responseApi.data.explanation)
-                        .addOnSuccessListener { translatedText ->
-                            println("Success Text Translation: $translatedText")
-                            _uiState.value = PictureOfTheDayViewState.Success(
-                                responseApi.data.copy(explanation = translatedText)
-                            )
-                        }
-                        .addOnFailureListener { exception ->
-                            println("Error Text Translation: ${exception.printStackTrace()}")
-                        }
+                    responseApi.data.explanation?.let {
+                        textTranslator.translate(it)
+                            .addOnSuccessListener { translatedText ->
+                                println("Success Text Translation: $translatedText")
+
+                                if (responseApi.data.mediaType == "video") _uiState.value =
+                                    PictureOfTheDayViewState.SuccessVideoUrl(
+                                        responseApi.data.copy(
+                                            explanation = translatedText
+                                        )
+                                    )
+                                else _uiState.value = PictureOfTheDayViewState.Success(
+                                    responseApi.data.copy(explanation = translatedText)
+                                )
+                            }
+                            .addOnFailureListener { exception ->
+                                println("Error Text Translation: ${exception.printStackTrace()}")
+                            }
+                    }
                 }
             }
         }
@@ -77,8 +119,9 @@ class PictureOfTheDayViewModel(
 }
 
 sealed interface PictureOfTheDayViewState {
-    data object Loading : PictureOfTheDayViewState
     data object Init : PictureOfTheDayViewState
+    data object Loading : PictureOfTheDayViewState
     data class Success(val image: PictureOfTheDay) : PictureOfTheDayViewState
+    data class SuccessVideoUrl(val videoUrl: PictureOfTheDay) : PictureOfTheDayViewState
     data class Error(val message: String) : PictureOfTheDayViewState
 }
