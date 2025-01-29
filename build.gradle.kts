e932d13b885cbe0222a97551502caf8ccf8d9bf1
walter <walter.d.retzer@gmail.com>
2025-01-29 18:22:57 -0300

    Adicionado libs do firebase remote config

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
