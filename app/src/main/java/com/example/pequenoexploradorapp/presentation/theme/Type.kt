c28459f7503816f1c7e45004e50ea4f066b1f871
walter <walter.d.retzer@gmail.com>
2025-02-03 23:18:44 -0300

    Fonts adicionadas ao Type

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/theme/Type.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/theme/Type.kt
@@ -2,33 +2,39 @@ package com.example.pequenoexploradorapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.pequenoexploradorapp.R
 
-// Set of Material typography styles to start with
+
+val QuickSand = FontFamily(
+    Font(R.font.quicksandbold, FontWeight.Bold),
+    Font(R.font.quicksandlight, FontWeight.Light),
+    Font(R.font.quicksandmedium, FontWeight.Medium),
+    Font(R.font.quicksandregular, FontWeight.Normal),
+    Font(R.font.quicksandsemibold, FontWeight.SemiBold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Bold,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Medium,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.SemiBold,
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = QuickSand,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Light,
     )
-    */
 )
