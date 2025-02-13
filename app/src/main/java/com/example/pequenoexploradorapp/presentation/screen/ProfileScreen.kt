4d82126e18169ceed8550a8f941c285c327d783d
walter <walter.d.retzer@gmail.com>
2025-02-13 20:06:53 -0300

    Ajuste de icone

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/ProfileScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.CheckCircleOutline
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Logout
