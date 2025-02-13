fc9e287374a78ad51fcac0f076c4606717b63493
walter <walter.d.retzer@gmail.com>
2025-02-13 16:57:22 -0300

    Ajuste da imagem do perfil

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/ProfileScreen.kt
@@ -1,14 +1,28 @@
 package com.example.pequenoexploradorapp.presentation.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.CheckCircleOutline
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -16,86 +30,308 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.pequenoexploradorapp.R
-import com.example.pequenoexploradorapp.presentation.components.AvatarImagesHorizontalPager
-import com.example.pequenoexploradorapp.presentation.components.MenuToolbar
-import com.example.pequenoexploradorapp.presentation.components.VerticalSpacer
+import androidx.compose.ui.unit.sp
+import com.example.pequenoexploradorapp.domain.secure.SharedPrefApp
+import com.example.pequenoexploradorapp.domain.secure.UserPreferences
+import com.example.pequenoexploradorapp.domain.util.ConstantsApp
+import com.example.pequenoexploradorapp.domain.util.formattedAsPhone
+import com.example.pequenoexploradorapp.domain.util.restartApp
+import com.example.pequenoexploradorapp.presentation.components.LoadingWithLine
+import com.example.pequenoexploradorapp.presentation.components.SimpleToolbar
+import com.example.pequenoexploradorapp.presentation.components.snackBarOnlyMessage
+import com.example.pequenoexploradorapp.presentation.components.snackBarWithActionButton
+import com.example.pequenoexploradorapp.presentation.viewmodel.ProfileViewModel
+import com.example.pequenoexploradorapp.presentation.viewmodel.ProfileViewState
+import org.koin.compose.koinInject
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(
+    onNavigateBack: () -> Unit,
+    onNavigateToChooseAvatar: () -> Unit,
+    viewModel: ProfileViewModel = koinInject(),
+    sharedPref: SharedPrefApp = koinInject()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val scope = rememberCoroutineScope()
     val snackBarHostState = remember { SnackbarHostState() }
-    val toolbarBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val uid: String = sharedPref.readString(UserPreferences.UID)
+    val name: String = sharedPref.readString(UserPreferences.NAME)
+    val email: String = sharedPref.readString(UserPreferences.EMAIL)
+    val phone: String = sharedPref.readString(UserPreferences.PHONE)
+    val image: Int = sharedPref.readInt(UserPreferences.IMAGE)
+    var isLoading by remember { mutableStateOf(false) }
 
 
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
         topBar = {
-            MenuToolbar(
-                title = "Perfil",
-                onNavigationToMenu = { },
-                onNavigationToProfile = { },
-                onNavigateToNotifications = { },
-                toolbarBehavior = toolbarBehavior,
-                isActivatedBadge = false
+            SimpleToolbar(
+                title = "Perfil $image",
+                onNavigationToBack = { onNavigateBack() },
+                onNavigationClose = { onNavigateBack() }
             )
-        },
-        containerColor = Color.Transparent
-    ) { paddingValues ->
+        }
+    ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(paddingValues)
-                .paint(
-                    painterResource(id = R.drawable.simple_background),
-                    contentScale = ContentScale.FillBounds
-                )
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceBetween,
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
         ) {
-            Column {
-                VerticalSpacer(32.dp)
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    IconButton(onClick = { }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description",
-                            tint = Color.White
-                        )
-                    }
-                    VerticalSpacer(10.dp)
-                    AvatarImagesHorizontalPager()
-                    VerticalSpacer(10.dp)
-                    IconButton(onClick = { }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description",
-                            tint = Color.White
-                        )
-                    }
-                }
-                VerticalSpacer(40.dp)
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                ) {
-                    Text(
-                        text = "Escolha seu Avatar",
-                        color = Color.White
-                    )
-                    VerticalSpacer(12.dp)
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(180.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color.White, CircleShape)
+                        .background(Color.Black, CircleShape)
+                        .clickable {
+                            onNavigateToChooseAvatar()
+                        }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 5.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = name,
+                    style = TextStyle(
+                        color = Color.Yellow,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        textAlign = TextAlign.Center
+                    ),
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    imageVector = Icons.Filled.Mail,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+                Text(
+                    text = email,
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        textAlign = TextAlign.Center
+                    ),
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    imageVector = Icons.Filled.Phone,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+                Text(
+                    text = phone.formattedAsPhone(),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    imageVector = Icons.Filled.CheckCircleOutline,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+                Text(
+                    text = "ID: $uid",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        textAlign = TextAlign.Center
+                    ),
+                )
+            }
+            LoadingWithLine(isLoading)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable {
+                            snackBarWithActionButton(
+                                coroutineScope = scope,
+                                snackBarHostState = snackBarHostState,
+                                message = ConstantsApp.MESSAGE_DELETE_ACCOUNT,
+                                actionLabel = "Excluir",
+                                onAction = { viewModel.onDeleteAccount() },
+                                onDismiss = { }
+                            )
+                        },
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+                Text(
+                    modifier = Modifier.clickable {
+                        snackBarWithActionButton(
+                            coroutineScope = scope,
+                            snackBarHostState = snackBarHostState,
+                            message = ConstantsApp.MESSAGE_DELETE_ACCOUNT,
+                            actionLabel = "Excluir",
+                            onAction = { viewModel.onDeleteAccount() },
+                            onDismiss = { }
+                        )
+                    },
+                    text = "Excluir Conta",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        textAlign = TextAlign.Center
+                    ),
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable {
+                            snackBarWithActionButton(
+                                coroutineScope = scope,
+                                snackBarHostState = snackBarHostState,
+                                message = ConstantsApp.MESSAGE_SIGN_OUT_ACCOUNT,
+                                actionLabel = "Sair",
+                                onAction = { viewModel.onSignOut() },
+                                onDismiss = { }
+                            )
+                        },
+                    imageVector = Icons.Default.Logout,
+                    contentDescription = null,
+                    tint = Color.White
+                )
+                Text(
+                    modifier = Modifier.clickable {
+                        snackBarWithActionButton(
+                            coroutineScope = scope,
+                            snackBarHostState = snackBarHostState,
+                            message = ConstantsApp.MESSAGE_SIGN_OUT_ACCOUNT,
+                            actionLabel = "Sair",
+                            onAction = { viewModel.onSignOut() },
+                            onDismiss = { }
+                        )
+                    },
+                    text = "Sair",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif,
+                        textAlign = TextAlign.Center
+                    ),
+                )
+            }
+            when (val state = uiState) {
+                is ProfileViewState.Dashboard -> {}
+
+                is ProfileViewState.Loading -> {
+                    isLoading = true
+                }
+
+                is ProfileViewState.Success -> {
+                    isLoading = false
+                    restartApp(context = LocalContext.current)
+                }
+
+                is ProfileViewState.SuccessClearInfoUser -> {
+                    snackBarOnlyMessage(
+                        snackBarHostState = snackBarHostState,
+                        coroutineScope = scope,
+                        message = state.message
+                    )
+                }
+
+                is ProfileViewState.Error -> {
+                    snackBarOnlyMessage(
+                        snackBarHostState = snackBarHostState,
+                        coroutineScope = scope,
+                        message = state.message
+                    )
                 }
             }
         }
