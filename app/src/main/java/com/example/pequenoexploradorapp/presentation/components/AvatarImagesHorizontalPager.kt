33730f6a4b5d005f2e479d57d36813be036ef99a
walter <walter.d.retzer@gmail.com>
2025-02-13 16:56:03 -0300

    Ajuste do pagerState

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/components/AvatarImagesHorizontalPager.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/components/AvatarImagesHorizontalPager.kt
@@ -8,41 +8,25 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.pequenoexploradorapp.R
+
 
 @Composable
-fun AvatarImagesHorizontalPager() {
-    val avatars = listOf(
-        R.drawable.perfil01,
-        R.drawable.perfil02,
-        R.drawable.perfil03,
-        R.drawable.perfil04,
-        R.drawable.perfil05,
-        R.drawable.perfil06,
-        R.drawable.perfil07,
-        R.drawable.perfil08,
-        R.drawable.perfil09,
-        R.drawable.perfil10,
-        R.drawable.perfil11,
-        R.drawable.perfil12,
-        R.drawable.perfil13,
-        R.drawable.perfil14,
-        R.drawable.perfil16,
-        R.drawable.perfil17,
-        R.drawable.perfil18,
-        R.drawable.perfil19,
-        R.drawable.perfil20
-    )
+fun AvatarImagesHorizontalPager(
+    pagerState: PagerState,
+    avatars: List<Int>
+) {
     val avatarSize = 180.dp
-    val pagerState = rememberPagerState(pageCount = { avatars.size })
 
     BoxWithConstraints {
         HorizontalPager(
