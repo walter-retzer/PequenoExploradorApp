4239d955bd012a6734be089dd353eafed2115f76
walter <walter.d.retzer@gmail.com>
2025-02-14 15:28:21 -0300

    Remove import

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/components/ProgressButton.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/components/ProgressButton.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.pequenoexploradorapp.presentation.theme.mainColor
 
 
