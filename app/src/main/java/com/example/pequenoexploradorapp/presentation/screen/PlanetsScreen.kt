d4d16ec715fab4277062f9e8511a18c513559b6d
walter <walter.d.retzer@gmail.com>
2025-02-14 17:40:12 -0300

    Ajuste de style do texto

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/PlanetsScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/PlanetsScreen.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -37,18 +33,17 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.pequenoexploradorapp.R
 import com.example.pequenoexploradorapp.data.Planet
 import com.example.pequenoexploradorapp.data.planets
 import com.example.pequenoexploradorapp.presentation.components.MenuToolbar
+import com.example.pequenoexploradorapp.presentation.components.VerticalSpacer
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlanetsScreen(
     onNavigateToLoadNasaImage: (imageSearch: String?) -> Unit,
 ) {
-    val stateScroll = rememberScrollState()
     val snackBarHostState = remember { SnackbarHostState() }
     val toolbarBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val listOfPlanets = planets
@@ -128,11 +123,10 @@ fun PlanetItem(planet: Planet) {
                 modifier = Modifier.fillMaxWidth(),
                 text = planet.name,
                 color = Color.White,
-                fontSize = 21.sp,
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.bodyLarge
             )
         }
     }
-    Spacer(Modifier.height(24.dp))
+    VerticalSpacer(24.dp)
 }
