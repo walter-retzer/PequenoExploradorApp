575de471732e210719de636cd29d5a25b2dc4a87
walter <walter.d.retzer@gmail.com>
2025-02-14 17:06:21 -0300

    Adicionado style de texto

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/theme/Type.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/theme/Type.kt
@@ -1,10 +1,13 @@
 package com.example.pequenoexploradorapp.presentation.theme
 
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.pequenoexploradorapp.R
 
 
@@ -20,21 +23,46 @@ val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = QuickSand,
         fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = QuickSand,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp
     ),
     bodySmall = TextStyle(
         fontFamily = QuickSand,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
     ),
     titleLarge = TextStyle(
         fontFamily = QuickSand,
         fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Light,
+        fontSize = 18.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp
     ),
     labelSmall = TextStyle(
         fontFamily = QuickSand,
         fontWeight = FontWeight.Light,
+        fontSize = 8.sp
     )
 )
