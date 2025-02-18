9d6a06e4da493d421a32a322a18fa454b2ab7355
walter <walter.d.retzer@gmail.com>
2025-02-18 18:01:08 -0300

    Ajuste dos icone favoritar

--- app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/PictureOfTheDayScreen.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/presentation/screen/PictureOfTheDayScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -303,11 +304,12 @@ fun RenderSuccessLoadImage(
                             .scale(scale)
                             .align(Alignment.TopEnd)
                             .padding(6.dp)
+                            .background(mainColor.copy(alpha = 0.5f), shape = CircleShape)
                     ) {
                         Icon(
                             imageVector = if (image.isFavourite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                             contentDescription = "Favorite Image from Picture Of the Day",
-                            tint = mainColor
+                            tint = Color.White
                         )
                     }
                 }
