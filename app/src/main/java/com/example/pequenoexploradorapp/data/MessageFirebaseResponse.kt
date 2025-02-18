a6a3dfd3ba1d4dd771d62fcc747aa5f750190614
walter <walter.d.retzer@gmail.com>
2025-02-18 12:06:47 -0300

    Adicionado class de resposta do Firebase Data Base

---
+++ app/src/main/java/com/example/pequenoexploradorapp/data/MessageFirebaseResponse.kt
@@ -0,0 +1,13 @@
+package com.example.pequenoexploradorapp.data
+
+data class MessageFirebaseResponse(
+//    val imageQuestion1: String = "",
+//    val imageQuestion2: String = "",
+//    val imageQuestion3: String = "",
+    val question1: String = "",
+    val question2: String = "",
+    val question3: String = "",
+    val answer1: String = "",
+    val answer2: String = "",
+    val answer3: String = "",
+)
