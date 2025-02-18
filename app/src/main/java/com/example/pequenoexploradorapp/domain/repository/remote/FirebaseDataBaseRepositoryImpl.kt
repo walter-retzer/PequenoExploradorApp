2387990412c2e64b9ea73058b095b8ecb3ff4623
walter <walter.d.retzer@gmail.com>
2025-02-18 12:08:56 -0300

    Implementado class FirebaseDataBaseRepositoryImpl

---
+++ app/src/main/java/com/example/pequenoexploradorapp/domain/repository/remote/FirebaseDataBaseRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.example.pequenoexploradorapp.domain.repository.remote
+
+import com.example.pequenoexploradorapp.data.MessageFirebaseResponse
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.getValue
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+
+class FirebaseDataBaseRepositoryImpl : FirebaseDataBaseRepository {
+
+    private val database = FirebaseDatabase.getInstance().reference
+    override fun getMessagesFlow(): Flow<MessageFirebaseResponse> = callbackFlow {
+        val messageRef = database.child("message")
+        try {
+            val listener = object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val response = snapshot.getValue<MessageFirebaseResponse>()
+                    println("$response")
+                    response?.let { trySend(it) }
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    println("$error")
+                    close(error.toException())
+                }
+            }
+            messageRef.addValueEventListener(listener)
+            awaitClose { messageRef.removeEventListener(listener) }
+        } catch (e: Exception) {
+            println("$e")
+        }
+    }
+}
