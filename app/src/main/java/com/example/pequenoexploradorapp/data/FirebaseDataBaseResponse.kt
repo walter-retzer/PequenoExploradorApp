6c8af31abca89e99ffce9b32c4c2a5bb5a849dc0
walter <walter.d.retzer@gmail.com>
2025-02-18 17:41:14 -0300

    Ajustado variaveis

--- app/src/main/java/com/example/pequenoexploradorapp/data/FirebaseDataBaseResponse.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/data/FirebaseDataBaseResponse.kt
@@ -1,13 +1,14 @@
 package com.example.pequenoexploradorapp.data
 
 data class FirebaseDataBaseResponse(
-//    val imageQuestion1: String = "",
-//    val imageQuestion2: String = "",
-//    val imageQuestion3: String = "",
+    val imageQuestionDefault: String = "",
+    val imageQuestion1: String = "",
+    val imageQuestion2: String = "",
+    val imageQuestion3: String = "",
     val question1: String = "",
     val question2: String = "",
     val question3: String = "",
     val answer1: String = "",
     val answer2: String = "",
-    val answer3: String = "",
+    val answer3: String = ""
 )
