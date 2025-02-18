07329fd096da91ef8bbc18ec430717be96c1d3ea
walter <walter.d.retzer@gmail.com>
2025-02-18 12:10:40 -0300

    Adicionado FirebaseDataBaseRepositoryImpl ao Koin

--- app/src/main/java/com/example/pequenoexploradorapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/pequenoexploradorapp/di/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.example.pequenoexploradorapp.di
 
 import com.example.pequenoexploradorapp.domain.datasource.local.FavouriteImageDao
 import com.example.pequenoexploradorapp.domain.repository.local.FavouriteImageRepositoryImpl
+import com.example.pequenoexploradorapp.domain.repository.remote.FirebaseDataBaseRepositoryImpl
 import com.example.pequenoexploradorapp.domain.repository.remote.RemoteRepositoryImpl
 import io.ktor.client.HttpClient
 import org.koin.dsl.module
@@ -9,6 +10,7 @@ import org.koin.dsl.module
 val repositoryModule = module {
     single { provideRemoteRepository(get()) }
     single { provideLocalFavouriteImageRepository(get()) }
+    single { FirebaseDataBaseRepositoryImpl() }
 }
 
 fun provideRemoteRepository(client: HttpClient): RemoteRepositoryImpl {
